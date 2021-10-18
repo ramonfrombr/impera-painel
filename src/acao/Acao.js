ff550b24ca438cff143fe2e4c96d63fa7dda17a7
ramonfrombr <ramonfrombr@gmail.com>
2021-10-18 18:06:09 -0300

    Painel de ações atualizado

--- src/acao/Acao.js
+++ src/acao/Acao.js
@@ -2,19 +2,25 @@ import React from "react";
 import "./acao.scss";
 import GraficoAcao from "../stock.svg";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 const Acao = ({ nome, preco }) => {
+	const acoes = "1";
 	return (
 		<div style={{ marginBottom: "5px", fontSize: "8px" }} className="acao">
 			<div className="acao__intro">
-				<span style={{ marginRight: "15px", fontSize: "16px" }}>{nome}</span>
+				<h3 style={{ marginRight: "15px", fontSize: "16px" }}>{nome}</h3>
+				<p className="acao__nAcoes">{acoes && acoes + " ações"}</p>
 			</div>
 			<div style={{ marginRight: "15px" }} className="acao__grafico">
 				<img src={GraficoAcao} height={16} />
 			</div>
-			<div className="acao__valor">
-				$ {preco} <ArrowUpwardIcon style={{ height: "12pt", color: "white" }} />
+
+			<div className="acao__numeros">
+				<p className="acao__preco">
+					$ {preco}{" "}
+					<ArrowUpwardIcon style={{ height: "12pt", color: "white" }} />
+				</p>
+				<p className="acao__porcentagem">+200%</p>
 			</div>
 		</div>
 	);
--- src/acoes/Acoes.js
+++ src/acoes/Acoes.js
@@ -4,12 +4,17 @@ import axios from "axios";
 import { staticBlock } from "@babel/types";
 import Acao from "../acao/Acao";
 
+// API TOKEN
 const TOKEN = "c5lu7vaad3if1qn1so3g";
+
+// API URL
 const BASE_URL = "https://finnhub.io/api/v1/quote";
 
 const Acoes = () => {
+	// Dados da sações
 	const [dadosAcoes, definirDadosAcoes] = useState([]);
 
+	// Seleciona os dados das ações no API
 	const selecionarDadosAcoes = (acao) => {
 		return axios
 			.get(`${BASE_URL}?symbol=${acao}&token=${TOKEN}`)
@@ -21,6 +26,7 @@ const Acoes = () => {
 	useEffect(() => {
 		let dados = [];
 
+		// Códigos das ações
 		const listaAcoes = [
 			"AAPL",
 			"MSFT",
@@ -32,8 +38,10 @@ const Acoes = () => {
 			"SBUX",
 		];
 
+		// Códigos das moedas
 		const listaMoedas = ["USD", "GBP", "JPY", "CAD", "EUR", "BRL"];
 
+		// Códigos das criptomoedas
 		const listaCripto = ["BTC", "ETH"];
 
 		//selecionarMinhasAcoes();
@@ -61,12 +69,6 @@ const Acoes = () => {
 	return (
 		<div className="acoes">
 			<div className="acoes__container">
-				<h3 className="secao__cabecalho">Bovespa/B3</h3>
-
-				<div className="acoes__conteudo"></div>
-			</div>
-
-			<div className="acoes__container">
 				<h3 className="secao__cabecalho">NYSE</h3>
 
 				<div className="acoes__conteudo">
@@ -79,6 +81,12 @@ const Acoes = () => {
 						/>
 					))}
 				</div>
+			</div>
+
+			<div className="acoes__container">
+				<h3 className="secao__cabecalho">Bovespa/B3</h3>
+
+				<div className="acoes__conteudo"></div>
 			</div>
 
 			<div className="acoes__container">
